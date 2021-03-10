71c8c520bf3193936f10273cc15af71fe914c491
Fernando Belmonte <fernando.belmonte@live.com>
2021-03-10 14:57:05 -0300

    update

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const getUsersInfo = async () => {
         password: ROCKETCHAT_PASSWORD,
     });
 
-    const rocketchat_getPermissionsAll = await rocketchat.get(
+    const rocketchat_getUserListAll_response = await rocketchat.get(
         "/api/v1/users.list",
         {
             headers: {
@@ -36,25 +36,37 @@ const getUsersInfo = async () => {
 
     const today = new Date();
 
-    const users = _.filter(rocketchat_getPermissionsAll.data.users, (user) => {
-        const lastLogin = new Date(user.lastLogin);
+    const users = _.filter(
+        rocketchat_getUserListAll_response.data.users,
+        (user) => {
+            const lastLogin = new Date(user.lastLogin);
 
-        return lastLogin < dateFns.subDays(today, DAYS);
-    });
+            return (
+                lastLogin < dateFns.subDays(today, DAYS) ||
+                (user.status === "online" &&
+                    lastLogin < dateFns.subDays(today, DAYS))
+            );
+        }
+    );
+
     console.log("\n---\n");
-    users.forEach(({ _id, username, name, lastLogin, createdAt, roles }) => {
-        const temp_lastLogin = formattedDate(new Date(lastLogin));
-        const temp_createAt = formattedDate(new Date(createdAt));
-        console.log({
-            _id,
-            username,
-            name,
-            lastLogin: temp_lastLogin,
-            createAt: temp_createAt,
-            roles,
-        });
-        console.log("\n---\n");
-    });
+
+    users.forEach(
+        ({ _id, username, name, lastLogin, createdAt, roles, status }) => {
+            const temp_lastLogin = formattedDate(new Date(lastLogin));
+            const temp_createAt = formattedDate(new Date(createdAt));
+            console.log({
+                _id,
+                username,
+                name,
+                lastLogin: temp_lastLogin,
+                createAt: temp_createAt,
+                roles,
+                status,
+            });
+            console.log("\n---\n");
+        }
+    );
 };
 
 getUsersInfo();
